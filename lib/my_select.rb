85278b55be8920180bc58c664f4abaa75fe12378
Yamuna Navada <yamunanavada@gmail.com>
2017-11-03 02:07:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+   i = 0
+   modified_coll = []
+   while i < collection.length
+     if yield collection[i]
+       modified_coll.push(collection[i])
+     end
+     i = i + 1
+   end
+ end
+
+ modified_coll
+
 end
